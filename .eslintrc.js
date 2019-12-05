3b459a94d1dd524ec32afc70b68ee66eb3a7e0e3
Colin Rotherham <work@colinr.com>
2019-12-05 15:05:20 +0000

    Add linting for path resolution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
 module.exports = {
   extends: [
     'standard',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings'
   ],
   settings: {
+    'import/extensions': [
+      '.js',
+      '.jsx',
+      '.mjs'
+    ],
     react: {
       version: 'detect'
     }
