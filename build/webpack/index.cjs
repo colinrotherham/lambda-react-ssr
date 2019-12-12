887418f3abea954d9d30839a1fe94bb538749c43
Colin Rotherham <work@colinr.com>
2019-12-12 09:52:29 +0000

    Compress JS output

--- build/webpack/index.cjs
+++ build/webpack/index.cjs
@@ -1,4 +1,5 @@
 const { NODE_ENV } = process.env
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = (env, argv) => {
   const config = require('./config.cjs')
@@ -7,6 +8,17 @@ module.exports = (env, argv) => {
   if (NODE_ENV === 'production') {
     config.devtool = 'nosources-source-map'
     config.mode = 'production'
+
+    // Compress output JS
+    config.optimization = {
+      minimize: true,
+      minimizer: [new TerserPlugin({
+        terserOptions: {
+          output: { comments: false }
+        },
+        extractComments: false
+      })]
+    }
   }
 
   return config
