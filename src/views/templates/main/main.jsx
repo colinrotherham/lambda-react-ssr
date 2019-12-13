c74b189674eb2b538d9db2b99bae5140bce8d5c8
Colin Rotherham <work@colinr.com>
2019-12-13 15:31:53 +0000

    Add CSS include

--- src/views/templates/main/main.jsx
+++ src/views/templates/main/main.jsx
@@ -10,6 +10,9 @@ function Template ({ title, children, lang }) {
 
         {/* Handheld support */}
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+
+        {/* CSS styles */}
+        <link rel="stylesheet" href="/assets/css/styles.css" />
       </head>
       <body>
         {children}
